d52188f156f431221208b1f7a0fd31228fcaca0e
YOONSUNGHYEON <qead753951@naver.com>
2021-08-23 20:55:58 +0900

    modified week4

--- Week4/방금그곡.java
+++ Week4/방금그곡.java
@@ -2,35 +2,38 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+
 class Solution {
-    static class Music implements Comparable<Music>{
-		int id,playingtime;
+	static class Music implements Comparable<Music> {
+		int id, playingtime;
 		String title, sheet;
 
-		Music(int id,int playingtime, String title, String sheet)
-		{
+		Music(int id, int playingtime, String title, String sheet) {
 			this.id = id;
 			this.playingtime = playingtime;
 			this.sheet = sheet;
 			this.title = title;
 		}
 
+		
+		//조건이 일치하는 음악이 여러 개일 때에는 라디오에서 재생된 시간이 제일 긴 음악 제목을 반환한다.
+		//재생된 시간도 같을 경우 먼저 입력된 음악 제목을 반환한다.
 		@Override
 		public int compareTo(Music m) {
 			if (this.playingtime < m.playingtime) {
-				return m.playingtime-this.playingtime;
-			} else if(this.playingtime == m.playingtime) {
-				if(this.id>m.id)
+				return m.playingtime - this.playingtime;
+			} else if (this.playingtime == m.playingtime) {
+				if (this.id > m.id)
 					return 1;
 				else
 					return -1;
-			}
-			else
+			} else
 				return -1;
 		}
 	}
-	
-    public static String check(String st) {
+
+	//"CC#BCC#BCC#BCC#B“ -> "CcBCcBCcBCcB"
+	public static String check(String st) {
 		st = st.replaceAll("A#", "a");
 		st = st.replaceAll("C#", "c");
 		st = st.replaceAll("D#", "d");
@@ -38,43 +41,52 @@ class Solution {
 		st = st.replaceAll("G#", "g");
 		return st;
 	}
-    public String solution(String m, String[] musicinfos) {
-        	String answer = "(None)";
-		m=check(m);
+	//m=네오가 기억한 멜로디
+	//musicinfos = 방송된 곡의 정보를 담고 있는 배열
+	public String solution(String m, String[] musicinfos) {
+		//조건이 일치하는 음악이 없을 때에는 “(None)”을 반환한다
+		String answer = "(None)";
+		m = check(m);
 		ArrayList<Music> list = new ArrayList<Music>();
-		for(int i=0; i<musicinfos.length;i++)
-		{
+		for (int i = 0; i < musicinfos.length; i++) {
 			String[] temp = musicinfos[i].split(",");
-			//재생시간 구하기
-			String[] start_time= temp[0].split(":");
-			String[] end_time=temp[1].split(":");
+			
+			String[] startTime = temp[0].split(":");
+			String[] endTime = temp[1].split(":");
 
-			int hour = Integer.parseInt(end_time[0])-Integer.parseInt(start_time[0]);
-			int minute = Integer.parseInt(end_time[1])-Integer.parseInt(start_time[1]);
-			list.add(new Music(i,(hour*60)+minute, temp[2], check(temp[3])));
+			// 재생시간 구하기
+			int hour = Integer.parseInt(endTime[0]) - Integer.parseInt(startTime[0]);
+			int minute = Integer.parseInt(endTime[1]) - Integer.parseInt(startTime[1]);
+			list.add(new Music(i, (hour * 60) + minute, temp[2], check(temp[3])));
 		}
-		ArrayList<Music> contain_list = new ArrayList<Music>();
-		for(int i=0; i<list.size();i++)
-		{
+		//네오가 기억한 멜로디를 포함하고 있는 곡 list
+		ArrayList<Music> containList = new ArrayList<Music>();
+		
+		for (int i = 0; i < list.size(); i++) {
 			StringBuilder builder = new StringBuilder();
-			int sheet_len=list.get(i).sheet.length();
-			int a =list.get(i).playingtime%sheet_len;
-			int b = list.get(i).playingtime/sheet_len;
-			for(int j=0; j<b; j++)
+			int sheet_len = list.get(i).sheet.length();
+			int a = list.get(i).playingtime % sheet_len;
+			int b = list.get(i).playingtime / sheet_len;
+			/*
+			 * CcBCcBCcB, 재생시간 8분
+			 * a : 8%9=8
+			 * b : 8/9=0
+			 * */
+			for (int j = 0; j < b; j++)
 				builder.append(list.get(i).sheet);
 			builder.append(list.get(i).sheet.substring(0, a));
-            if(builder.toString().contains(m))
-				contain_list.add(list.get(i));
+			if (builder.toString().contains(m))
+				containList.add(list.get(i));
 
 		}
-		Collections.sort(contain_list, new Comparator<Music>() {
+		Collections.sort(containList, new Comparator<Music>() {
 			@Override
 			public int compare(Music s1, Music s2) {
 				return s1.compareTo(s2);
 			}
 		});
-		if(contain_list.size()>0)
-			answer  = contain_list.get(0).title;
-        return answer;
-    }
+		if (containList.size() > 0)
+			answer = containList.get(0).title;
+		return answer;
+	}
 }
